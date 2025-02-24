1acd2829e5a397e78dc11106c5b76ba0db54cd89
mooji <70057651+moooooji@users.noreply.github.com>
2025-02-25 00:41:30 +0900

    modify proxy contract

--- src/NFTAuctionProxy.sol
+++ src/NFTAuctionProxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "./ERC1967Proxy.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
 
 contract NFTAuctionProxy is ERC1967Proxy {
