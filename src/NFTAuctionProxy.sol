293a51a7e0a0b4661501cd937b55dac5640922b4
mooji <70057651+moooooji@users.noreply.github.com>
2025-02-28 04:10:55 +0900

    fix state variable in NFTAuctionProxy.sol

--- src/NFTAuctionProxy.sol
+++ src/NFTAuctionProxy.sol
@@ -7,6 +7,33 @@ import "forge-std/console.sol";
 
 contract NFTAuctionProxy is ERC1967Proxy {
 
+    enum AuctionState {Created, Active, Ended}
+
+    struct Auction {
+        address seller;
+        address nftAddress;
+        uint256 tokenId;
+        uint256 minPrice;
+        AuctionState state;
+    }
+
+    mapping(uint256 => Auction) public listings;
+    
+    mapping(address => uint256) public totalBalance;
+    mapping(address => uint256) public lowerBid;
+    mapping(address => uint256) public higherBid;
+    mapping(address => uint256[]) public playersBid;
+
+
+    uint256 public listingFee;
+    uint256 public startTime;
+    uint256 public tokenId;
+    uint256 public currentBid;
+    address public highestBidder;
+    bool private isStop;
+    address public admin;
+    bool public isMulticallExecution;
+
     constructor(address _implementationAddr, bytes memory _data) ERC1967Proxy(_implementationAddr, _data) {
         console.log("parameter imple: ", _implementationAddr);
         console.log("setted imple: ",_implementation());
